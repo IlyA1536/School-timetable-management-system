03124afdc4057244d0b4d211c1f5e2c82db38d76
IlyA1536 <i0977187143@gmailcom>
2024-02-10 23:36:20 +0200

    Did all function and add 2 new table: Schedule, Grade

--- SchoolSystem/models.py
+++ SchoolSystem/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class Subject(models.Model):
     subject = models.CharField(max_length=100, unique=True)
+
     teacher = models.ManyToManyField("Teacher")
 
     def __str__(self):
@@ -17,9 +20,11 @@ class Teacher(models.Model):
     def __str__(self):
         return f"{self.name} {self.surname}"
 
+
 class Class(models.Model):
     title = models.CharField(max_length=3, unique=True)
     specialization = models.CharField(max_length=100)
+
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -31,7 +36,29 @@ class Student(models.Model):
     surname = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     age = models.IntegerField()
+
     student_class = models.ForeignKey(Class, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.name} {self.surname}"
+
+
+class Schedule(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    clas = models.ForeignKey(Class, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.date, self.subject, self.clas, self.teacher}"
+
+
+class Grade(models.Model):
+    grade = models.IntegerField(validators = [MinValueValidator(12), MinValueValidator(1)])
+
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.grade, self.student, self.schedule}"
